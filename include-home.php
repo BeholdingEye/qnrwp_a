41f2a05c564ad30eafc9a572ecb9d047784de10f
Beholding Eye <BeholdingEye@users.noreply.github.com>
2017-02-06 22:35:38 +0100

    Add files via upload

---
+++ include-home.php
@@ -0,0 +1,22 @@
+<?php
+$rT = <<<EOT
+<div id="promo" class="qnr-responsive">
+  <div>
+    <span class="qnr-glyph-icons qnr-glyph-flag promo-glyph"></span>
+    <h1>Simple yet Powerful</h1>
+    <p>And expandable theme suited for custom code, not patches. Free Quicknr Interface widgets are used instead of third-party interface plugins.</p>
+  </div>
+  <div>
+    <span class="qnr-glyph-icons qnr-glyph-gears-be promo-glyph"></span>
+    <h1>Wordpress Native Features</h1>
+    <p>This theme uses standard Wordpress features to full effect. Categories, Widgets, Shortcodes and more. If Wordpress can do it, this theme can harness it.</p>
+  </div>
+  <div>
+    <span class="qnr-glyph-icons qnr-glyph-team promo-glyph"></span>
+    <h1>Free, not Premium</h1>
+    <p>This is a GPL licenced theme, open source and free of charge. It aims to offer complete functionality, with no need for paid upgrades.</p>
+  </div>
+</div>
+EOT;
+return $rT;
+?>
